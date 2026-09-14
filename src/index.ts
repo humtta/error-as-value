6e10d1d38e7997776cda0aa2b05b2bf4af9728a7
Hugo Marotta <humtta@proton.me>
2026-09-13 23:24:27 -0300

    Add err function
    err creates a failed Result.

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,7 @@ export interface Err<E> {
 export function ok<T>(value: T): Ok<T> {
   return { ok: true, value };
 }
+
+export function err<E>(error: E): Err<E> {
+  return { ok: false, error };
+}
